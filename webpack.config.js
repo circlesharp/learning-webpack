f59b36c7e042bc8959f391bf4cf68feb2993725c
circlesharp <circlesharp@live.com>
2020-06-04 11:46:16 +0800

    zanshi

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    r
+  }
+}
