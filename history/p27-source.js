6339770f78c07c2460727804c7dbb47c1c5496cd
circlesharp <circlesharp@live.com>
2020-06-05 18:48:45 +0800

    新建rewriteWebpack

--- history/p27-source.js
+++
@@ -1,1 +0,0 @@
-export default 'the source from source.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,10 @@
 const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: './src/index.js',
   output: {
-    filename: '[name].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: true,
-    contentBase: './dist'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_module/,
-      include: path.resolve(__dirname, 'src'),
-      use: {
-        loader: 'babel-loader',
-        options: { presets: [ '@babel/preset-env' ] }
-      }
-    }]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './public/index.html' }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  }
 }
